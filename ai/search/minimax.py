ee366750eaefbd4b3f72d9bc2347a95bedcc39f2
Jenswill <61697954+Jenswill@users.noreply.github.com>
2022-03-08 11:33:45 +0100

    added minimax.py

---
+++ ai/search/minimax.py
@@ -0,0 +1,15 @@
+class minimax():
+    def __init__(self, game, state) -> None:
+        pass
+
+    def search():
+        pass
+
+    def max_val():
+        pass
+
+    def min_val():
+        pass
+    
+
+
