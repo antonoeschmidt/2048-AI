251f29f5b765cbc4c67e1c070317ff7c1b1d64b0
Jens <jenswilliversen@gmail.com>
2022-03-12 13:55:54 +0100

    added agent and program.py
    Co-Authored-By: siffravn <43875054+siffravn@users.noreply.github.com>

---
+++ ai/program.py
@@ -0,0 +1,13 @@
+class Program():
+    def __init__(self, problem = None, search = None):
+        self.problem = problem
+
+    def __call__(self, percept):
+        ## Returns the action desired to play
+        pass
+
+    def update_state():
+        pass
+
+
+
