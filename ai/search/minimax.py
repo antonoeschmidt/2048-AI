619c8b84a4f48cecb9029f43e154ec9dc6c14faa
Jens <jenswilliversen@gmail.com>
2022-03-12 15:16:14 +0100

    fully implemented minimax
    Co-Authored-By: siffravn <43875054+siffravn@users.noreply.github.com>

--- ai/search/minimax.py
+++ ai/search/minimax.py
@@ -5,28 +5,39 @@ class minimax():
 
     def __call__(self, game, state):
         self.player = game.to_move()
-        self.max_val(game, state)
-        pass
+        value,move = self.max_val(game, state)
+        return move
+
 
     def max_val(self,game, state, depth):
         if game.is_terminal() or depth > self.max_depth:
             return game.utility(state, self.player)
-        v = -np.inf
-        children = []
+
+        v, move = -np.inf
         actions = game.actions(state)
         
         for action in actions:
             new_state = game.result(state,action)
-            children.append(new_state)
-            self.min_val(game, state, depth)
+            v2,a2 = self.min_val(game, new_state, depth)
+
+            if v2 > v:
+                v,move = v2,action
+
+        return v, move
+
 
     def min_val(self, game, state, depth):
         if game.is_terminal():
            return game.utility(state, self.player)
 
-        v = np.inf
-        children = game.spawm(state)
-        
-        for child in children:
-           self.max_val(game,child,depth+1)
+        v, move = np.inf
+        actions = game.spawm_actions(state)
 
+        for action in actions:
+           new_state = game.result(state,action)
+           v2, a2 = self.max_val(game,new_state,depth+1)
+
+           if v2 < v:
+               v,move = v2,action
+        
+        return v, move
